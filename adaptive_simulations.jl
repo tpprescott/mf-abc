36a3ece201af3cb1c026cd3de6fcda44dabf42fa
tpprescott <tpprescott@gmail.com>
2019-05-29 18:23:11 +0100

    Track progress of viral adaptive simulations

--- adaptive_simulations.jl
+++ adaptive_simulations.jl
@@ -3,9 +3,14 @@ using Distributed
 
 bm = MakeBenchmarkCloud("viral/output")
 idx = Iterators.partition(1:length(bm), 1000)
+println("--- Loaded benchmark")
 
-adaptive_mfabc(i) = MakeMFABCCloud(Viral.mf_prob, bm[i], (0.4,0.4), method="mf", lower_eta=0.1, budget=2*cost(bm[i]))
-mfabc_set = pmap(adaptive_mfabc, idx)
+pmap(i->MakeMFABCCloud(Viral.mf_prob, bm[[i]], (0.4,0.4), method="mf", lower_eta=0.1, budget=2*cost(bm[[i]])), 1:nworkers());
+println("--- Good to go")
+
+mfabc_set = pmap(i->MakeMFABCCloud(Viral.mf_prob, bm[i], (0.4,0.4), method="mf", lower_eta=0.1, budget=2*cost(bm[i])), idx)
+println("--- Completed extra simulations")
+println("--- Writing files")
 
 for i in 1:length(mfabc_set)
 write_cloud(mfabc_set[i], "viral/output/mf/cloud$i")
