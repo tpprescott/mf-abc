b209cc16f3652d31ae7c800de0851f060c3ba0bf
tpprescott <tpprescott@gmail.com>
2019-06-05 09:52:34 +0100

    scripted Fig2 size and both large/small burn-in sizes

--- adaptive_simulations.jl
+++ adaptive_simulations.jl
@@ -9,10 +9,20 @@ println("--- Loaded benchmark")
 pmap(i->MakeMFABCCloud(Viral.mf_prob, bm[[i]], epsilons, method="mf", lower_eta=0.01, budget=1.01*cost(bm[[i]])), 1:nworkers());
 println("--- Good to go")
 
+println("--- Small burn-in")
 mfabc_set = pmap(i->MakeMFABCCloud(Viral.mf_prob, bm[i], epsilons, method="mf", lower_eta=0.01, budget=2*cost(bm[i])), idx)
-println("--- Completed extra simulations")
+println("--- Completed extra simulations: small burn-in")
 println("--- Writing files")
-
 for i in 1:length(mfabc_set)
-write_cloud(mfabc_set[i], "viral/output/mf/cloud$i")
+ write_cloud(mfabc_set[i], "viral/output/mf_smallBI/cloud$i")
 end
+
+println("--- Large burn-in")
+mfabc_set = pmap(i->MakeMFABCCloud(Viral.mf_prob, bm, epsilons, method="mf", lower_eta=0.01, budget=cost(bm)+cost(bm[i])), idx)
+println("--- Completed extra simulations: large burn-in")
+println("--- Writing files")
+for i in 1:length(mfabc_set)
+ write_cloud(mfabc_set[i], "viral/output/mf_largeBI/cloud$i")
+end
+
+
--- figure_script.jl
+++ figure_script.jl
@@ -17,7 +17,7 @@ savefig(plot(fig1a, fig1b, layout=2, size=(900,360)), "figures/fig1.pdf")
 println("# Fig 2 and Table 1")
 fig2a = compare_efficiencies(bm, sample_size, epsilons, output="theory")
 fig2b, table1, latex_table1 = compare_efficiencies(bm, sample_size, epsilons, output="plot")
-savefig(plot(fig2a, fig2b, layout=2, size=(1000,400)), "figures/fig2.pdf")
+savefig(plot(fig2a, fig2b, layout=2, size=(1100,440)), "figures/fig2.pdf")
 
 println("# Table 2 and 3")
 variances, phis, etas = observed_variances(bm, sample_size, epsilons, Repressilator.F, [100.0, 200.0, 300.0])
@@ -36,7 +36,9 @@ println("# Fig 3")
 savefig(view_distances(bm[1:10000], epsilons, epsilons.*2), "figures/fig3.pdf")
 
 println("# Fig 4")
-savefig(plot_eta_estimates(mf_set, bm, epsilons; method="mf", lower_eta=eta_0), "figures/fig4.pdf")
+fig4 = plot_eta_estimates(mf_set, bm, epsilons; method="mf", lower_eta=eta_0)
+plot!(xlim=(0,0.4),ylim=(0,0.4))
+savefig(fig4, "figures/fig4.pdf")
 
 println("# Fig 5")
 bm_set = [cld[1:sample_size] for cld in mf_set]
