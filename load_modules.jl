8542c4872d2b10c94259719b39d6bae8a72841eb
Tom Prescott --- Home <tpprescott@gmail.com>
2020-04-23 19:23:29 +0100

    Fit electrotaxis into new formatting

--- load_modules.jl
+++ load_modules.jl
@@ -1,6 +1,19 @@
 include("likelihood_free.jl")
-include("abc.jl")
-include("mf.jl")
+include("applications/electro/ElectroTaxis.jl")
 using .LikelihoodFree
-using .ABC
-using .MFABC
+using .ElectroTaxis
+using LinearAlgebra, Distributed, Distances, Distributions, DifferentialEquations, IndexedTables, Plots
+
+prior = DistributionGenerator(SingleCellModel_NoEF, product_distribution([
+    Uniform(0,2), Uniform(0,5), Uniform(0,5), Uniform(0,2)
+]))
+K = PerturbationKernel{SingleCellModel_NoEF}(MvNormal(zeros(4), diagm(fill(0.1, 4))))
+F = SingleCellSimulator(σ_init=0.1)
+L_abc = ABCLikelihood(F, Euclidean(), 1.0, num_simulations=1*50) # integer multiple of 50
+L_bsl = BayesianSyntheticLikelihood(F, num_simulations=50) # ideally 500, although can be slow
+
+Σ_is = MonteCarloProposal(prior, L_bsl, NoEF_displacements)
+Σ_mcmc = MonteCarloProposal(prior, PerturbationKernel{SingleCell}, L_bsl, NoEF_displacements)
+
+# importance_sample(Σ_is, length, 10; loglikelihood=false)
+# mcmc_sample(Σ_mcmc, length, 10)
