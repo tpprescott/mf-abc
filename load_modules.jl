b29e3539c9166b7fd294dead53ad1e09b699bcd7
Tom Prescott --- Home <tpprescott@gmail.com>
2020-04-24 14:33:48 +0100

    Numbers of simulations and typo fixed in the loading script

--- load_modules.jl
+++ load_modules.jl
@@ -10,10 +10,10 @@ prior = DistributionGenerator(SingleCellModel_NoEF, product_distribution([
 K = PerturbationKernel{SingleCellModel_NoEF}(MvNormal(zeros(4), diagm(0=>fill(0.1, 4))))
 F = SingleCellSimulator(σ_init=0.1)
 L_abc = ABCLikelihood(F, Euclidean(), 1.0, num_simulations=1*50) # integer multiple of 50
-L_bsl = BayesianSyntheticLikelihood(F, num_simulations=50) # ideally 500, although can be slow
+L_bsl = BayesianSyntheticLikelihood(F, num_simulations=500) # ideally >500, although can be slow
 
 Σ_is = MonteCarloProposal(prior, L_bsl, NoEF_displacements)
-Σ_mcmc = MonteCarloProposal(prior, PerturbationKernel{SingleCell}, L_bsl, NoEF_displacements)
+Σ_mcmc = MonteCarloProposal(prior, K, L_bsl, NoEF_displacements)
 
 # importance_sample(Σ_is, length, 10; loglikelihood=false)
 # mcmc_sample(Σ_mcmc, length, 10)
