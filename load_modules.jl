a8e23aacd92bd9dc9450dcb66b837e23c726ac5e
Tom Prescott --- Home <tpprescott@gmail.com>
2020-04-24 14:26:26 +0100

    Backport LinearAlgebra diagm

--- load_modules.jl
+++ load_modules.jl
@@ -7,7 +7,7 @@ using LinearAlgebra, Distributed, Distances, Distributions, DifferentialEquation
 prior = DistributionGenerator(SingleCellModel_NoEF, product_distribution([
     Uniform(0,2), Uniform(0,5), Uniform(0,5), Uniform(0,2)
 ]))
-K = PerturbationKernel{SingleCellModel_NoEF}(MvNormal(zeros(4), diagm(fill(0.1, 4))))
+K = PerturbationKernel{SingleCellModel_NoEF}(MvNormal(zeros(4), diagm(0=>fill(0.1, 4))))
 F = SingleCellSimulator(σ_init=0.1)
 L_abc = ABCLikelihood(F, Euclidean(), 1.0, num_simulations=1*50) # integer multiple of 50
 L_bsl = BayesianSyntheticLikelihood(F, num_simulations=50) # ideally 500, although can be slow
