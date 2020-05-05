05163334845573af9ae59fd2724e65c07aacdf05
Tom Prescott --- Home <tpprescott@gmail.com>
2020-05-05 17:23:29 +0100

    Scripted rejection sampling

--- applications/electro/ElectroTaxisSingleCellAnalysis.jl
+++ applications/electro/ElectroTaxisSingleCellAnalysis.jl
@@ -18,8 +18,10 @@ const F_NoEF = SingleCellSimulator(σ_init=0.1)
 const L_NoEF_BSL = BayesianSyntheticLikelihood(F_NoEF, numReplicates=50)
 const L_NoEF_CSL = BayesianSyntheticLikelihood(F_NoEF, numReplicates=50, numIndependent=1)
 
-const Σ_NoEF_BSL = MCMCProposal(prior_NoEF, K_NoEF, L_NoEF_BSL, y_obs_NoEF)
-const Σ_NoEF_CSL = MCMCProposal(prior_NoEF, K_NoEF, L_NoEF_CSL, y_obs_NoEF)
+export Σ_NoEF_BSL_MC, Σ_NoEF_BSL_IS, Σ_NoEF_CSL_MC
+const Σ_NoEF_BSL_MC = MCMCProposal(prior_NoEF, K_NoEF, L_NoEF_BSL, y_obs_NoEF)
+const Σ_NoEF_BSL_IS = ISProposal(prior_NoEF, L_NoEF_BSL, y_obs_NoEF)
+const Σ_NoEF_CSL_MC = MCMCProposal(prior_NoEF, K_NoEF, L_NoEF_CSL, y_obs_NoEF)
 
 
 # EF
@@ -35,21 +37,15 @@ const F_EF = SingleCellSimulator(σ_init=0.1, angles=true, emf = EMF)
 const L_EF_BSL = BayesianSyntheticLikelihood(F_EF, numReplicates=50)
 const L_EF_CSL = BayesianSyntheticLikelihood(F_EF, numReplicates=50, numIndependent=1)
 
-const Σ_EF_BSL = MCMCProposal(prior_EF, K_EF, L_EF_BSL, y_obs_EF)
-const Σ_EF_CSL = MCMCProposal(prior_EF, K_EF, L_EF_CSL, y_obs_EF)
+export Σ_EF_BSL_MC, Σ_EF_BSL_IS, Σ_EF_CSL_MC
+const Σ_EF_BSL_MC = MCMCProposal(prior_EF, K_EF, L_EF_BSL, y_obs_EF)
+const Σ_EF_BSL_IS = ISProposal(prior_EF, L_EF_BSL, y_obs_EF)
+const Σ_EF_CSL_MC = MCMCProposal(prior_EF, K_EF, L_EF_CSL, y_obs_EF)
 
 # Joint
-const Σ_Joint_CSL = MCMCProposal(prior_EF, K_EF, (L_NoEF_CSL, L_EF_CSL), (y_obs_NoEF, y_obs_EF))
-
-export MCMC_NoEF_BSL, MCMC_NoEF_CSL
-MCMC_NoEF_BSL(N) = Iterators.take(Σ_NoEF_BSL, N)
-MCMC_NoEF_CSL(N) = Iterators.take(Σ_NoEF_CSL, N)
-
-export MCMC_EF_BSL, MCMC_EF_CSL
-MCMC_EF_BSL(N) = Iterators.take(Σ_EF_BSL, N)
-MCMC_EF_CSL(N) = Iterators.take(Σ_EF_CSL, N)
-
-export MCMC_Joint_CSL
-MCMC_Joint_CSL(N) = Iterators.take(Σ_EF_BSL, N)
+export Σ_Joint_BSL_MC, Σ_Joint_BSL_IS, Σ_Joint_CSL_MC
+const Σ_Joint_BSL_MC = MCMCProposal(prior_EF, K_EF, (L_NoEF_BSL, L_EF_BSL), (y_obs_NoEF, y_obs_EF))
+const Σ_Joint_BSL_IS = ISProposal(prior_EF, (L_NoEF_BSL, L_EF_BSL), (y_obs_NoEF, y_obs_EF))
+const Σ_Joint_CSL_MC = MCMCProposal(prior_EF, K_EF, (L_NoEF_CSL, L_EF_CSL), (y_obs_NoEF, y_obs_EF))
 
 end
