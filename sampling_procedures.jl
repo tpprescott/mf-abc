cddccf6e97dc4fdfc2feb79d5f78c40f2e369401
Tom Prescott --- Home <tpprescott@gmail.com>
2020-05-05 17:35:59 +0100

    Enable alternative constructors for IS

--- sampling_procedures.jl
+++ sampling_procedures.jl
@@ -113,10 +113,39 @@ struct ISProposal{Θ<:AbstractModel, Π<:AbstractGenerator{Θ}, Q<:AbstractGener
     function ISProposal(prior::Π, q::Q, lh_set::W) where Π <: AbstractGenerator{Θ} where Q<:AbstractGenerator{Θ} where Θ where W
         return new{Θ, Π, Q, W}(prior, q, lh_set)
     end
-    function ISProposal(prior::Π, lh_set::W) where Π<:AbstractGenerator{Θ} where Θ where W
-        return new{Θ, Π, Π, W}(prior, prior, lh_set)
-    end
 end
+
+ISProposal(
+    prior::AbstractGenerator{Θ},
+    q::AbstractGenerator{Θ},
+    L_set::NTuple{N, L where L<:AbstractLikelihoodFunction}, 
+    y_obs_set::NTuple{N, X where X}
+) where N where Θ = ISProposal(prior, q, (L_set, y_obs_set))
+
+ISProposal(
+    prior::AbstractGenerator{Θ}, 
+    q::AbstractGenerator{Θ},
+    lh_fun::LikelihoodObservationPair...
+) where Θ = ISProposal(prior, q, zip(lh_fun...)...)
+
+ISProposal(
+    prior::AbstractGenerator{Θ}, 
+    q::AbstractGenerator{Θ},
+    w::AbstractLikelihoodFunction, 
+    y_obs,
+) where Θ = ISProposal(prior, q, ((w,), (y_obs,)))
+
+ISProposal(
+    prior::AbstractGenerator{Θ}, 
+    args...,
+) where Θ = ISProposal(prior, prior, args...)
+
+function ISProposal(prior::AbstractGenerator{Θ}, q::AbstractGenerator{Θ}, args...) where Θ
+    error("$(args...) is not valid for creating an IS proposal")
+end
+
+
+
 function propose(
     Σ::ISProposal{Θ, Π, Q, LikelihoodObservationSet{N, TLL, TXX}}, 
     args...; kwargs...) where Π where Q where TLL where TXX where Θ where N
