caa20d5a61f843462732c8976acf227a1f43795c
Tom Prescott --- Home <tpprescott@gmail.com>
2020-05-07 09:24:49 +0100

    SMC wrapper implemented

--- synthetic_bayes.jl
+++ synthetic_bayes.jl
@@ -6,20 +6,21 @@ export BayesianSyntheticLikelihood
 struct BayesianSyntheticLikelihood{F, Y<:NamedTuple, SimOptions<:NamedTuple} <: LikelihoodFreeLikelihoodFunction{F}
     f::F
     y::Array{Y,1}
+    scale::Int64 # log (synthetic) likelihood is divided by 2^scale to create intermediate distributions between prior and synthetic_likelihood*prior
     kw::SimOptions
 
-    function BayesianSyntheticLikelihood(f::F, y::Array{Y,1}; numReplicates::Int64=length(y), kwargs...) where F<:AbstractSimulator where Y
+    function BayesianSyntheticLikelihood(f::F, y::Array{Y,1}, scale::Int64=0; numReplicates::Int64=length(y), kwargs...) where F<:AbstractSimulator where Y
         kw = merge((numReplicates=numReplicates,), kwargs)
-        return new{F, eltype(F), typeof(kw)}(f, y, kw)
+        return new{F, eltype(F), typeof(kw)}(f, y, scale, kw)
     end
 end
 
-function BayesianSyntheticLikelihood(f::F; numReplicates::Int64, kwargs...) where F <: AbstractSimulator
+function BayesianSyntheticLikelihood(f::F, scale=0; numReplicates::Int64, kwargs...) where F <: AbstractSimulator
     Y = eltype(F)
-    return BayesianSyntheticLikelihood(f, Array{Y,1}(); numReplicates=numReplicates, kwargs...)
+    return BayesianSyntheticLikelihood(f, Array{Y,1}(), scale; numReplicates=numReplicates, kwargs...)
 end
-function BayesianSyntheticLikelihood(L::BayesianSyntheticLikelihood{F}, y::Array{Y,1}; kwargs...) where F where Y
-    return BayesianSyntheticLikelihood(L.f, y; L.kw..., kwargs...)
+function BayesianSyntheticLikelihood(L::BayesianSyntheticLikelihood{F}, y::Array{Y,1}, scale=0; kwargs...) where F where Y
+    return BayesianSyntheticLikelihood(L.f, y, scale; L.kw..., kwargs...)
 end
 
 function (L::BayesianSyntheticLikelihood)(θ::AbstractModel, args...; kwargs...)
@@ -31,12 +32,13 @@ function (L::BayesianSyntheticLikelihood)(y_obs; loglikelihood::Bool, kwargs...)
 
     t = table(L.y)
     y = select(t, :y) # Requires output of simulation for comparison with data to be called "y"
-    length(y)//length(y_obs) >= 10 || (@warn "Only $(length(y)) simulations for $(length(y_obs)) observations.")
+    length(y)//length(y_obs) >= 10 || (@warn "Only $(length(y)) simulations for $(length(y_obs)) observations." maxlog=1)
 
     μ = mean(y)
     Σ = cov(y)
     sb_lh = MvNormal(μ, Σ)
 
     logw = sum(logpdf.(Ref(sb_lh), y_obs))
+    logw /= (2^L.scale)
     return loglikelihood ? logw : exp(logw)
 end
