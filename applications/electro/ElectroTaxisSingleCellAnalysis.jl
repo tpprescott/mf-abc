64fe8467e831ce6051d90d1706ebed684e0cd676
Tom Prescott --- Home <tpprescott@gmail.com>
2020-05-08 11:35:27 +0100

    Scripting numbers for EF and Joint

--- applications/electro/ElectroTaxisSingleCellAnalysis.jl
+++ applications/electro/ElectroTaxisSingleCellAnalysis.jl
@@ -18,19 +18,27 @@ const F_NoEF = SingleCellSimulator(σ_init=0.1)
 L_NoEF_BSL(s, n) = BayesianSyntheticLikelihood(F_NoEF, s, numReplicates=n)
 L_NoEF_CSL(s, n, i) = BayesianSyntheticLikelihood(F_NoEF, s, numReplicates=n, numIndependent=i)
 
-export smc_generations
-smc_generations(T::Int64, N::Int64) = smc_generations(T-1:-1:0, fill(N,T))
-smc_generations(s::AbstractArray{Int64,1}, n::AbstractArray{Int64,1}) = smc_generations(zip(s, n)...)
-smc_generations(tup::Tuple) = (((L_NoEF_BSL(tup...), ), (y_obs_NoEF,)),)
-smc_generations(tup::Tuple, tups::Tuple...) = (smc_generations(tup)..., smc_generations(tups...)...)
-
 
 export Σ_NoEF_BSL_MC, Σ_NoEF_BSL_IS, Σ_NoEF_BSL_SMC, Σ_NoEF_CSL_MC
+const Σ_NoEF_BSL_MC = MCMCProposal(prior_NoEF, K_NoEF, L_NoEF_BSL(0, 500), y_obs_NoEF)
+const Σ_NoEF_BSL_IS = ISProposal(prior_NoEF, L_NoEF_BSL(0, 500), y_obs_NoEF)
+const Σ_NoEF_BSL_SMC = SMCWrapper(
+    prior_NoEF,
+    (
+        ((L_NoEF_BSL(9,  50),), (y_obs_NoEF,)),
+        ((L_NoEF_BSL(8, 100),), (y_obs_NoEF,)),
+        ((L_NoEF_BSL(7, 150),), (y_obs_NoEF,)),
+        ((L_NoEF_BSL(6, 200),), (y_obs_NoEF,)),
+        ((L_NoEF_BSL(5, 250),), (y_obs_NoEF,)),
+        ((L_NoEF_BSL(4, 300),), (y_obs_NoEF,)),
+        ((L_NoEF_BSL(3, 350),), (y_obs_NoEF,)),
+        ((L_NoEF_BSL(2, 400),), (y_obs_NoEF,)),
+        ((L_NoEF_BSL(1, 450),), (y_obs_NoEF,)),
+        ((L_NoEF_BSL(0, 500),), (y_obs_NoEF,)),
+    )
+)
+const Σ_NoEF_CSL_MC = MCMCProposal(prior_NoEF, K_NoEF, L_NoEF_CSL(0, 500, 5), y_obs_NoEF)
 
-Σ_NoEF_BSL_MC(s, n) = MCMCProposal(prior_NoEF, K_NoEF, L_NoEF_BSL(s, n), y_obs_NoEF)
-Σ_NoEF_BSL_IS(s, n) = ISProposal(prior_NoEF, L_NoEF_BSL(s, n), y_obs_NoEF)
-Σ_NoEF_BSL_SMC(s, n) = SMCWrapper(prior_NoEF, smc_generations(s, n))
-Σ_NoEF_CSL_MC(s,n,i) = MCMCProposal(prior_NoEF, K_NoEF, L_NoEF_CSL(s, n, i), y_obs_NoEF)
 
 # EF
 const EMF = ConstantEF(1.0)
@@ -42,18 +50,48 @@ const K_EF = PerturbationKernel{SingleCellModel_EF}(
 )
 const y_obs_EF = vcat.(EF_displacements, EF_angles)
 const F_EF = SingleCellSimulator(σ_init=0.1, angles=true, emf = EMF)
-const L_EF_BSL = BayesianSyntheticLikelihood(F_EF, numReplicates=50)
-const L_EF_CSL = BayesianSyntheticLikelihood(F_EF, numReplicates=50, numIndependent=1)
+L_EF_BSL(s,n) = BayesianSyntheticLikelihood(F_EF, s, numReplicates=n)
+L_EF_CSL(s,n,i) = BayesianSyntheticLikelihood(F_EF, s, numReplicates=n, numIndependent=i)
 
-export Σ_EF_BSL_MC, Σ_EF_BSL_IS, Σ_EF_CSL_MC
-const Σ_EF_BSL_MC = MCMCProposal(prior_EF, K_EF, L_EF_BSL, y_obs_EF)
-const Σ_EF_BSL_IS = ISProposal(prior_EF, L_EF_BSL, y_obs_EF)
-const Σ_EF_CSL_MC = MCMCProposal(prior_EF, K_EF, L_EF_CSL, y_obs_EF)
+export Σ_EF_BSL_MC, Σ_EF_BSL_IS, Σ_EF_BSL_SMC, Σ_EF_CSL_MC
+const Σ_EF_BSL_MC = MCMCProposal(prior_EF, K_EF, L_EF_BSL(0, 500), y_obs_EF)
+const Σ_EF_BSL_IS = ISProposal(prior_EF, L_EF_BSL(0, 500), y_obs_EF)
+const Σ_EF_BSL_SMC = SMCWrapper(
+    prior_EF,
+    (
+        ((L_EF_BSL(9,  50),), (y_obs_EF,)),
+        ((L_EF_BSL(8, 100),), (y_obs_EF,)),
+        ((L_EF_BSL(7, 150),), (y_obs_EF,)),
+        ((L_EF_BSL(6, 200),), (y_obs_EF,)),
+        ((L_EF_BSL(5, 250),), (y_obs_EF,)),
+        ((L_EF_BSL(4, 300),), (y_obs_EF,)),
+        ((L_EF_BSL(3, 350),), (y_obs_EF,)),
+        ((L_EF_BSL(2, 400),), (y_obs_EF,)),
+        ((L_EF_BSL(1, 450),), (y_obs_EF,)),
+        ((L_EF_BSL(0, 500),), (y_obs_EF,)),
+    )
+)
+const Σ_EF_CSL_MC = MCMCProposal(prior_EF, K_EF, L_EF_CSL(0, 500, 5), y_obs_EF)
 
 # Joint
-export Σ_Joint_BSL_MC, Σ_Joint_BSL_IS, Σ_Joint_CSL_MC
-const Σ_Joint_BSL_MC = MCMCProposal(prior_EF, K_EF, (L_NoEF_BSL(0, 50), L_EF_BSL), (y_obs_NoEF, y_obs_EF))
-const Σ_Joint_BSL_IS = ISProposal(prior_EF, (L_NoEF_BSL(0, 50), L_EF_BSL), (y_obs_NoEF, y_obs_EF))
-const Σ_Joint_CSL_MC = MCMCProposal(prior_EF, K_EF, (L_NoEF_CSL(0, 50,1), L_EF_CSL), (y_obs_NoEF, y_obs_EF))
+export Σ_Joint_BSL_MC, Σ_Joint_BSL_IS, Σ_Joint_CSL_MC, Σ_Joint_BSL_SMC
+const Σ_Joint_BSL_MC = MCMCProposal(prior_EF, K_EF, (L_NoEF_BSL(0, 500), L_EF_BSL(0, 500)), (y_obs_NoEF, y_obs_EF))
+const Σ_Joint_BSL_IS = ISProposal(prior_EF, (L_NoEF_BSL(0, 500), L_EF_BSL(0, 500)), (y_obs_NoEF, y_obs_EF))
+const Σ_Joint_BSL_SMC = SMCWrapper(
+    prior_EF,
+    (
+        ((L_NoEF_BSL(9,  50), L_EF_BSL(9,  50)), (y_obs_NoEF, y_obs_EF)),
+        ((L_NoEF_BSL(8, 100), L_EF_BSL(8, 100)), (y_obs_NoEF, y_obs_EF)),
+        ((L_NoEF_BSL(7, 150), L_EF_BSL(7, 150)), (y_obs_NoEF, y_obs_EF)),
+        ((L_NoEF_BSL(6, 200), L_EF_BSL(6, 200)), (y_obs_NoEF, y_obs_EF)),
+        ((L_NoEF_BSL(5, 250), L_EF_BSL(5, 250)), (y_obs_NoEF, y_obs_EF)),
+        ((L_NoEF_BSL(4, 300), L_EF_BSL(4, 300)), (y_obs_NoEF, y_obs_EF)),
+        ((L_NoEF_BSL(3, 350), L_EF_BSL(3, 350)), (y_obs_NoEF, y_obs_EF)),
+        ((L_NoEF_BSL(2, 400), L_EF_BSL(2, 400)), (y_obs_NoEF, y_obs_EF)),
+        ((L_NoEF_BSL(1, 450), L_EF_BSL(1, 450)), (y_obs_NoEF, y_obs_EF)),
+        ((L_NoEF_BSL(0, 500), L_EF_BSL(0, 500)), (y_obs_NoEF, y_obs_EF)),
+    )
+)
+const Σ_Joint_CSL_MC = MCMCProposal(prior_EF, K_EF, (L_NoEF_CSL(0, 500 , 5), L_EF_CSL(0, 500, 5)), (y_obs_NoEF, y_obs_EF))
 
 end
