3baf86989c0f0a1abc41c4133a2ddb65c9d58318
Tom Prescott --- Home <tpprescott@gmail.com>
2020-06-30 18:45:37 +0100

    Allow for train/test data split with optional test_idx keyword

--- smc.jl
+++ smc.jl
@@ -2,14 +2,16 @@
 
 export SMCWrapper
 
-mutable struct SMCWrapper{N, Θ, Π<:AbstractGenerator{Θ}, W<:NTuple{N, LikelihoodObservationSet}}
+mutable struct SMCWrapper{N, M, Θ, Π<:AbstractGenerator{Θ}, W<:NTuple{N, LikelihoodObservationSet}}
     prior::Π
     lh_set::W
     numSimulations::NTuple{N, Int64}
     scale::NTuple{N, Float64}
+    test_idx::NTuple{M, Array{Int64,1}}
     function SMCWrapper(prior::Π, lh_set::W) where W where Π<:AbstractGenerator{Θ} where Θ
         N = length(lh_set)
-        Σ = new{N, Θ, Π, W}()
+        M = length(lh_set[end][1])
+        Σ = new{N, M, Θ, Π, W}()
         Σ.prior = prior
         Σ.lh_set = lh_set
         return Σ
@@ -23,9 +25,9 @@ SequentialImportanceDistribution(
 ) = SequentialImportanceDistribution(select(tn, :θ), select(tn, :weight), prior, delta)
 
 function Base.iterate(
-    Σ::SMCWrapper{N, Θ},
+    Σ::SMCWrapper{N, M, Θ},
     (n, qn)::Tuple{Int64, AbstractGenerator{Θ}} = (0, Σ.prior)
-) where N where Θ
+) where N where M where Θ
 
     if n>=N
         return nothing
@@ -34,8 +36,7 @@ function Base.iterate(
         println("Generation $n of $N")
         
         Σn = ISProposal(Σ.prior, qn, Σ.lh_set[n])
-        tn = importance_sample(Σn, Σ.numSimulations[n], scale=Σ.scale[n])
-        
+        tn = importance_sample(Σn, Σ.numSimulations[n], scale=Σ.scale[n], test_idx=Σ.test_idx)
         qn1 = n==N ? qn : SequentialImportanceDistribution(tn, Σ.prior)
         return tn, (n, qn1)
     end
@@ -46,12 +47,14 @@ Base.eltype(::Type{SMC}) where SMC<:SMCWrapper = IndexedTable
 
 export smc_sample
 function smc_sample(
-    Σ::SMCWrapper{N},
-    numSimulations::NTuple{N,Int64},
+    Σ::SMCWrapper{N, M},
+    numSimulations::NTuple{N,Int64};
     scale::NTuple{N,Number} = Tuple(Iterators.repeated(1, N)),
-    ) where N
+    test_idx::NTuple{M,AbstractArray{Int64,1}} = Tuple(Iterators.repeated(Array{Int64,1}(), M))
+    ) where N where M
 
-    Σ.scale = scale
     Σ.numSimulations = numSimulations
+    Σ.scale = scale
+    Σ.test_idx = test_idx
     return collect(Σ)
 end
